88be13d5e1707f485a9b5b4f1c689a5f5e534314
Terry <terry@appincloud.cn>
2022-01-11 18:50:56 +0800

    fix the unpack issue

--- usd_helper.py
+++ usd_helper.py
@@ -14,20 +14,20 @@ def generate_prim(path, vecs):
 
     cc = 0
     for i in range(len(vecs)):
-        subset = UsdGeom.Subset.Define(stage, path + "/subset" + str(i))
-        subset.CreateElementTypeAttr().Set("face")
-        subset_indices = [i]
+        #subset = UsdGeom.Subset.Define(stage, path + "/subset" + str(i))
+        #subset.CreateElementTypeAttr().Set("face")
+        #subset_indices = [i]
         #rel = subset.GetPrim().CreateRelationship("material:binding", False)
         #rel.SetTargets([Sdf.Path(material_scope_path + "/OmniPBR" + str(i))])
         positions = vecs[i]
         for j in range(len(positions)):
             gf = Gf.Vec3f(positions[j][0], positions[j][1], positions[j][2])
             points.append(gf)
-            normals.append(gf.Normalized())
+            normals.append(gf.GetNormalized())
             indices.append(cc)
             cc += 1
             
-        subset.CreateIndicesAttr().Set(subset_indices)
+        #subset.CreateIndicesAttr().Set(subset_indices)
         vertexCounts.append(len(vecs))
 
     mesh.CreateFaceVertexCountsAttr().Set(vertexCounts)
@@ -40,23 +40,22 @@ def read_data(datafile):
     import carb
     ret = []
     with open(datafile, 'rb') as fp:
-        n = struct.unpack('i', fp.read(4))
-        for i in range(n):
-            strands_len = struct.unpack('i', fp.read(4))
+        n = struct.unpack('i', fp.read(4))[0]
+        for i in range(1000):
+            strands_len = struct.unpack('i', fp.read(4))[0]
             positions = []
             for j in range(strands_len):
-                x = struct.unpack('f', fp.read(4))
-                y = struct.unpack('f', fp.read(4))
-                z = struct.unpack('f', fp.read(4))
-                carb.log_info(x,y,z)
-                positions.append([x,y,z])
+                x,y,z = struct.unpack('fff', fp.read(12))
+                #y = struct.unpack('f', fp.read(4))
+                #z = struct.unpack('f', fp.read(4))
+                positions.append([x*100,y*100,z*100])
             ret.append(positions)
     return ret
 
 def convert_usd(datafile, usdfile):
     # set up axis to z
     UsdGeom.SetStageUpAxis(stage, UsdGeom.Tokens.z)
-    UsdGeom.SetStageMetersPerUnit(stage, 0.01)
+    UsdGeom.SetStageMetersPerUnit(stage, 1)
 
     defaultPrimPath = str(stage.GetDefaultPrim().GetPath())# Triangle mesh with multiple materials
     path = defaultPrimPath + "/triangleMesh"
@@ -65,4 +64,4 @@ def convert_usd(datafile, usdfile):
 
     generate_prim(path, vecs)
 
-convert_usd("C:\\Users\\terry\\Documents\\Datasets\\strands00001.data", "C:\\Users\\terry\\Documents\\Datasets\\strands00001.usd")
+convert_usd("strands00001.data", "strands00001.usd")
