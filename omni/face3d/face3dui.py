a910f5b83aa42b60ee61e558336639b350ea77ea
terry <terry@appincloud.cn>
2022-01-18 22:38:08 +0800

    scale

--- omni/face3d/face3dui.py
+++ omni/face3d/face3dui.py
@@ -5,7 +5,7 @@ import os
 import time
 from omni.kit.widget.settings import create_setting_widget, create_setting_widget_combo, SettingType
 import omni.ui as ui
-from pxr import Gf
+from pxr import Gf,UsdGeom
 import asyncio
 from . import filehelper
 
@@ -81,11 +81,35 @@ class Face3dUI:
                 usd_filename = filename + ".usd"
                 usd_filepath = os.path.join(os.path.join(outpath, filename), usd_filename)
                 await filehelper.convert(obj_filepath, usd_filepath)
-                self.insert_into_scene(usd_filepath)
+                self.insert_into_scene(usd_filepath, 1)
 
-    def insert_into_scene(self, usd_filepath):
+    def insert_into_scene(self, usd_filepath, scale):
         stage = omni.usd.get_context().get_stage()
         prim_path = str(stage.GetDefaultPrim().GetPath())
         over_path = prim_path + "/face"
         over = stage.OverridePrim(over_path)
         over.GetReferences().AddReference(usd_filepath)
+
+        metersPerUnit = UsdGeom.GetStageMetersPerUnit(stage)
+        scaled_scale = scale / metersPerUnit
+        pos = Gf.Vec3d(0,0,0)
+        rot = Gf.Rotation(Gf.Vec3d(0,0,0), 0)
+        addobject_fn(over.GetPath(), pos, rot, scaled_scale)
+        
+# Got this From Lou Rohan... Thanks Lou!
+# objectpath - path in omniverse - omni:/Projects/Siggraph2019/AtticWorkflow/Props/table_cloth/table_cloth.usd
+# objectname - name you want it to be called in the stage
+# xform - Gf.Matrix4d
+def addobject_fn(path, position, rotation, scale):
+    # The original model was translated by the centroid, and scaled to be normalized by the length of the
+    # hypotenuse of the bbox
+    translate_mtx = Gf.Matrix4d()
+    rotate_mtx = Gf.Matrix4d()
+    scale_mtx = Gf.Matrix4d()
+
+    translate_mtx.SetTranslate(position)  # centroid/metersPerUnit)
+    rotate_mtx.SetRotate(rotation)
+    scale_mtx = scale_mtx.SetScale(scale)
+    transform_matrix = scale_mtx * rotate_mtx * translate_mtx
+
+    omni.kit.commands.execute("TransformPrimCommand", path=path, new_transform_matrix=transform_matrix)
